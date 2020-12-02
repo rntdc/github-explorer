84815ad06252ef03b47a202ae2fd08cbee3e0697
Renato <rntdek@gmail.com>
2020-12-02 19:24:19 -0300

    estilizacao dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
-import { Title} from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form action="">
+                <input placeholder="Nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img src="https://avatars2.githubusercontent.com/u/49010570?s=460&u=7c40a5565f85aa4bb187ffb315083d754b0755ec&v=4" alt=""></img>
+                    <div>
+                        <strong>rntdc/coronametter</strong>
+                        <p>Dados atualizados sobre o coronavirus</p>
+                    </div>
+                    
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="test">
+                    <img src="https://avatars2.githubusercontent.com/u/49010570?s=460&u=7c40a5565f85aa4bb187ffb315083d754b0755ec&v=4" alt=""></img>
+                    <div>
+                        <strong>rntdc/coronametter</strong>
+                        <p>Dados atualizados sobre o coronavirus</p>
+                    </div>
+                    
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="test">
+                    <img src="https://avatars2.githubusercontent.com/u/49010570?s=460&u=7c40a5565f85aa4bb187ffb315083d754b0755ec&v=4" alt=""></img>
+                    <div>
+                        <strong>rntdc/coronametter</strong>
+                        <p>Dados atualizados sobre o coronavirus</p>
+                    </div>
+                    
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="test">
+                    <img src="https://avatars2.githubusercontent.com/u/49010570?s=460&u=7c40a5565f85aa4bb187ffb315083d754b0755ec&v=4" alt=""></img>
+                    <div>
+                        <strong>rntdc/coronametter</strong>
+                        <p>Dados atualizados sobre o coronavirus</p>
+                    </div>
+                    
+                    <FiChevronRight size={20}/>
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,18 +9,22 @@ export default createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
+
   body {
     background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
+
   body, input, button {
     font: 16px Roboto, sans-serif;
   }
+
   #root {
     max-width: 960px;
     margin: 0 auto;
     padding: 40px 20px;
   }
+  
   button {
     cursor: pointer;
   }
